6da45eab68cdbd5c5e1b6038caafc9f9335e2da6
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-09-25 17:43:01 -0300

    criando a entidade e tabela Paciente

---
+++ IntelectahClinic/Models/Paciente.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace IntelectahClinic.Models;
+
+public class Paciente : IdentityUser
+{
+    [Required]
+    public string Cpf { get; set; }
+    [Required]
+    public DateTime DataNascimento { get; set; }
+
+    public Paciente() : base() { }
+}
---
+++ IntelectahClinic/Repository/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using IntelectahClinic.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelectahClinic.Repository;
+
+public class UserContext : IdentityDbContext<Paciente>
+{
+    public UserContext(DbContextOptions<UserContext> options) : base(options)
+    {
+        
+    }
+}
